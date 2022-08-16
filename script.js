f18aa8ab73c5c9819d87ece3d3da08623093708d
JimJamLee <li.james.chien@gmail.com>
2022-08-15 19:38:17 -0700

    updated html and started js

---
+++ script.js
@@ -0,0 +1,9 @@
+var searchEl = document.getElementById("search-btn");
+var filmEl = document.getElementById("film-input");
+
+searchEl.addEventListener("click", function(event) {
+
+})
+function renderSearch() {
+
+}
