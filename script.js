0f8ba910ac231ee5c4a6195b6f9e861e922ddce8
JimJamLee <li.james.chien@gmail.com>
2022-08-16 20:14:17 -0700

    Updated script.js with search button function

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 var searchEl = document.getElementById("search-btn");
 var filmEl = document.getElementById("film-input");
 
-searchEl.addEventListener("click", function(event) {
-
-})
 function renderSearch() {
+    var queryURL = "https://www.omdbapi.com/?s=" + filmEl.value + "&apikey=8e725623"
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then (function(response) {
+            console.log(queryURL);
+            console.log(response);
 
+        //     var searchResultEls = document.querySelectorAll(".box");
+        //     for (i=0; i < searchResultEls.length; i++) {
+        //         searchResultEls[i].media.media-content.content.movie-title1.innerHTML = response.search[0].title
+        //     }
+        })
 }
+
+searchEl.addEventListener("click", function(event) {
+    const searchFilm = filmEl.value;
+    renderSearch(searchFilm);
+})
