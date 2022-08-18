7779942acb554cb2760ef4c1de4e251cdeb9ea19
markkoos <markusimercado@gmail.com>
2022-08-18 16:26:11 -0700

    fixed bug with dailymotion API

--- script.js
+++ script.js
@@ -44,10 +44,10 @@ function renderSearch() {
         })
 }
 
-searchEl.addEventListener("click", function(event) {
-    const searchFilm = filmEl.value;
-    renderSearch(searchFilm);
-})
+// searchEl.addEventListener("click", function(event) {
+//     const searchFilm = filmEl.value;
+//     renderSearch(searchFilm);
+// })
 
 
 
@@ -138,37 +138,5 @@ searchEl.addEventListener("click", function(event) {
 
 
 
-// Dailymotion API 
- function getDailyAPI() {
-// fetch request for Dailymotion
-        // includes a placeholder for the movie title we get from the OMDb API
-    var dailyRequestURL = `https://api.dailymotion.com/videos?fields=id,title&search=whiplash+trailer+ov&limit=1`
 
-    var trailerData = ""
-
-    fetch(dailyRequestURL)
-    .then(function (response) {
-        console.log(response);
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        var trailerData = data.list[0].id
-        function addScript() {
-            var script = document.createElement( 'script' );
-            script.setAttribute(`src`, `https://geo.dailymotion.com/player.js`);
-            script.setAttribute(`data-video`, `${trailerData}`);
-            document.body.appendChild(script);
-          }
-        
-          addScript();
-        console.log(trailerData);
-        
-    })
-
-    
-
- }
-
- getDailyAPI();
 
